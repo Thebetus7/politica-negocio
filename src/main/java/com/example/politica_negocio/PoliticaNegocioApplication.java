7e7fdae051efdb9c0726070cde623560a41e15cd
edberto <edybanera@gmail.com>
2026-04-08 12:17:08 -0400

    first commit

---
+++ src/main/java/com/example/politica_negocio/PoliticaNegocioApplication.java
@@ -0,0 +1,13 @@
+package com.example.politica_negocio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticaNegocioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticaNegocioApplication.class, args);
+	}
+
+}
